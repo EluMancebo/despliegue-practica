b43c3800a953739b176e56f79cbfc95b10361f58
EluM <emango0298@gmail.com>
2024-10-12 00:05:52 +0200

    Usuario 1 - Hotfix: mejora en la sección de contenido HTML

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function cambiarEstilo() {
   document.getElementById("miTexto").style.color = "white";
   document.getElementById("miTexto").style.fontSize = "20px";
   document.getElementById("miTexto").style.backgroundColor = "brown";
-  document.getElementById("miTexto").style.inlineSize = "230px";
+  document.getElementById("miTexto").style.width = "230px";
+  document.getElementById("miTexto").style.textAlign = "center";
 }
 function cambiarImagen() {
   document.getElementById("miImagen").src = "images/informaticogafas.jpg";
