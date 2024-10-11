9962db689cbd5003363ce3f070be99f186231034
EluM <emango0298@gmail.com>
2024-10-11 21:09:49 +0200

    Usuario 2  - Añadir contenido HTML

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function cambiarEstilo() {
   document.getElementById("miTexto").style.color = "white";
   document.getElementById("miTexto").style.fontSize = "20px";
   document.getElementById("miTexto").style.backgroundColor = "brown";
-  document.getElementById("miTexto").style.inlineSize = "200px";
+  document.getElementById("miTexto").style.inlineSize = "230px";
 }
