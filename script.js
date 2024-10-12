48cf0163535a889b2c84afce0b41277fa456d5bb
EluM <emango0298@gmail.com>
2024-10-12 11:59:27 +0200

    Corregir sintaxis de margin en cambiarEstilo y arreglo enlace Imagen

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function cambiarEstilo() {
   document.getElementById("miTexto").style.backgroundColor = "brown";
   document.getElementById("miTexto").style.width = "230px";
   document.getElementById("miTexto").style.textAlign = "center";
+  document.getElementById("miTexto").style.margin = "0 auto";
+  document.getElementById("miTexto").style.display = "block";
 }
 function cambiarImagen() {
-  document.getElementById("miImagen").src = "images/informaticogafas.jpg";
+  document.getElementById("miImagen").src = "images/Informaticogafas.jpg";
 }
